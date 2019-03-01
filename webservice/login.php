02ec940906c73dee0659f7d631aa649857e656ee
merandatakougang <merandatakougang@gmail.com>
2019-03-01 11:50:27 +0100

    modification de  la  fonction  de  login  pour  pouvoir  renvoyer  les  donnees  en json

--- webservice/login.php
+++ webservice/login.php
@@ -1,17 +1,21 @@
 <?php
 include 'db_function.php';
+require_once 'db_config.php';
+global $pdo;
 $response = array();
 
 if (isset($_REQUEST['username']) && isset($_REQUEST['password'])){
     $username = $_REQUEST['username'];
     $password=$_REQUEST['password'];
-
     $result=login($username,$password);
     if ($result) {
-        $user=$result->fetch(PDO::FETCH_ASSOC);
+        $exist=$pdo->prepare('SELECT * FROM users  WHERE username=?');
+        $exist->execute([$username]);
+        $user=$exist->fetch(PDO::FETCH_OBJ);
         $response["success"] = 1;
         $response["message"] = "sucessfull login";
-        $response["username"]=$username;
+        $response["username"]=$user->username;
+        $response["id"]=$user->id;
         echo json_encode($response);
     }
     else {
