26fb531f674f2f12cf31a2d018ba41301074114e
ruthel <balockruthel.cedrickascens@gmail.com>
2019-03-01 16:47:54 +0100

    Interface de Login v2
    Amélioration du design  l'interface de login

--- Traplare/app/src/main/java/com/example/ruthelpc/traplare/LoginActivity.java
+++ Traplare/app/src/main/java/com/example/ruthelpc/traplare/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.ruthelpc.traplare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
     }
 }
