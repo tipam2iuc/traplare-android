abd5e5f450cf95e56f0a77c2784ea77aaac80459
ruthel <balockruthel.cedrickascens@gmail.com>
2019-03-01 12:52:31 +0100

    begining interface

---
+++ Traplare/gradlew.bat
@@ -0,0 +1,45 @@
+<application>
+  <component name="ActionsCollector">
+    <counts>
+      <entry action="$Copy" count="24" />
+      <entry action="$Cut" count="4" />
+      <entry action="$Delete" count="48" />
+      <entry action="$Paste" count="41" />
+      <entry action="$Redo" count="1" />
+      <entry action="$SelectAll" count="17" />
+      <entry action="$Undo" count="1493" />
+      <entry action="ActivateProjectToolWindow" count="4" />
+      <entry action="ActivateTerminalToolWindow" count="1" />
+      <entry action="Android Profiler" count="1" />
+      <entry action="Android.CreateResourceDirectory" count="1" />
+      <entry action="Android.CreateSampleDataDirectory" count="1" />
+      <entry action="Android.RunAndroidAvdManager" count="9" />
+      <entry action="Android.RunAndroidSdkManager" count="126" />
+      <entry action="Android.ShowThemeEditor" count="1" />
+      <entry action="Android.SyncProject" count="9" />
+      <entry action="ChooseRunConfiguration" count="2" />
+      <entry action="CleanGradleProject" count="3" />
+      <entry action="CloseAllEditors" count="1" />
+      <entry action="CodeCompletion" count="382" />
+      <entry action="ContextHelp" count="1" />
+      <entry action="CopyElement" count="6" />
+      <entry action="CopyPaths" count="2" />
+      <entry action="CopyReference" count="1" />
+      <entry action="Debug" count="280" />
+      <entry action="Debugger.EditFrameSource" count="1" />
+      <entry action="DialogOkAction Entrée" count="97" />
+      <entry action="EditorBackSpace" count="3862" />
+      <entry action="EditorChooseLookupItem" count="785" />
+      <entry action="EditorChooseLookupItemReplace" count="615" />
+      <entry action="EditorCopy" count="219" />
+      <entry action="EditorCut" count="847" />
+      <entry action="EditorDelete" count="532" />
+      <entry action="EditorDeleteLine" count="99" />
+      <entry action="EditorDeleteToWordStart" count="3" />
+      <entry action="EditorDown" count="1899" />
+      <entry action="EditorDuplicate" count="29" />
+      <entry action="EditorEnter" count="917" />
+      <entry action="EditorEscape" count="8" />
+      <entry action="EditorIndentSelection" count="6" />
+      <entry action="EditorLeft" count="666" />
+      <entry action="EditorLineEn
