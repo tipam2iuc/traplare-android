b1441dd4142b1a03e2b63b432223eb8e9513d7fe
merandatakougang <merandatakougang@gmail.com>
2019-03-02 04:30:37 +0100

    ajout des attributs  pour  pouvoir recuperer les messages du server disant

--- Traplare/app/src/main/java/com/example/ruthelpc/traplare/users.java
+++ Traplare/app/src/main/java/com/example/ruthelpc/traplare/users.java
@@ -12,6 +12,25 @@ public class users {
     private String cni;
     @SerializedName("password")
     private String password;
+    @SerializedName("message")
+    private String message;
+    @SerializedName("success")
+    private String success;
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
 
     public String getUsername() {
         return username;
