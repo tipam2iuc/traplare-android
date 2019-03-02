6200873f68f5284a1facb41ce5d269c9848f5b4f
ruthel <balockruthel.cedrickascens@gmail.com>
2019-03-02 06:42:08 +0100

    modification du register

--- Traplare/app/src/main/java/com/example/ruthelpc/traplare/RegisterActivity.java
+++ Traplare/app/src/main/java/com/example/ruthelpc/traplare/RegisterActivity.java
@@ -2,6 +2,8 @@ package com.example.ruthelpc.traplare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         getSupportActionBar().hide();
+
+        Spinner spin = findViewById(R.id.spinner_type_piece);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_identity_piece,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+
     }
 }
