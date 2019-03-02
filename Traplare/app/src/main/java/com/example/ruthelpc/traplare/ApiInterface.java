4cfe70a07190248fd0a8c1f84bff2db7534bbb55
merandatakougang <merandatakougang@gmail.com>
2019-03-02 04:25:25 +0100

    interfacede mapping et d'appel des  methodes distantes

--- Traplare/app/src/main/java/com/example/ruthelpc/traplare/ApiInterface.java
+++ Traplare/app/src/main/java/com/example/ruthelpc/traplare/ApiInterface.java
@@ -1,5 +1,17 @@
 package com.example.ruthelpc.traplare;
 
-public interface ApiInterface {
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
 
+public interface ApiInterface {
+    @FormUrlEncoded
+    @POST("/inscription.php")
+    Call<users> inscription(
+            @Field("username") String username,
+            @Field("name") String name,
+            @Field("cni") String cni,
+            @Field("password") String password
+    );
 }
