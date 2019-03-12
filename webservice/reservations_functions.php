8d6f26c16c7e57662a8919eaf7d92f066f9f6b89
merandatakougang <merandatakougang@gmail.com>
2019-03-12 05:03:55 +0100

    fonctions de reservations

--- webservice/reservations_functions.php
+++ webservice/reservations_functions.php
@@ -1,1 +1,18 @@
 <?php
+require_once "db_config.php;";
+
+function add_reservertion($idvoyage,$date_reservation,$confirmation,$iduser,$nom,$prenom,$cni,$phone)
+{
+
+    global  $pdo;
+    if (!empty($idvoyage)&& !empty($date_reservation)&& !empty($confirmation)&& !empty($iduser)
+    && !empty($nom)&& !empty($prenom)&&!empty($cni) && !empty($phone)
+    )
+    {
+         $req =$pdo->prepare
+         (
+             'INSERT INTO reservation VALUES (null ,?,?,?,?,?,?,?,?)'
+         );
+       return  $req->execute([$idvoyage,$date_reservation,$confirmation,$iduser,$nom,$prenom,$cni,$phone]);
+    }
+}
