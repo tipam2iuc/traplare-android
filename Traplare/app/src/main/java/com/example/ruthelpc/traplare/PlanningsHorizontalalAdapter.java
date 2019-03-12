5ebeb109295d264001dc44097a35155d1d7c1814
ruthel <balockruthel.cedrickascens@gmail.com>
2019-03-12 15:36:34 +0100

    formulaire de reservation

--- Traplare/app/src/main/java/com/example/ruthelpc/traplare/PlanningsHorizontalalAdapter.java
+++ Traplare/app/src/main/java/com/example/ruthelpc/traplare/PlanningsHorizontalalAdapter.java
@@ -2,6 +2,7 @@ package com.example.ruthelpc.traplare;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
@@ -30,18 +31,6 @@ public class PlanningsHorizontalalAdapter extends RecyclerView.Adapter<Plannings
         View view = null;
         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext().getApplicationContext());
         view = inflater.inflate(R.layout.planning_travels,viewGroup,false);
-        if (PlanningsVerticalAdapter.wellLoad == true) {
-            ConstraintLayout c = view.findViewById(R.id.layout_template_travel2);
-            c.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Dialog dialog = new Dialog(context);
-                    dialog.setContentView(R.layout.custom_dialog);
-                    dialog.setCanceledOnTouchOutside(true);
-                    dialog.show();
-                }
-            });
-        }
         Log.i("CreateHolder","Holder Created");
         return new PlanningsHolder(view);
     }
@@ -54,6 +43,13 @@ public class PlanningsHorizontalalAdapter extends RecyclerView.Adapter<Plannings
             planningsHolder.textView_destination.setText(voyagesListe.get(position).getDestination());
             planningsHolder.textView_categorie.setText(voyagesListe.get(position).getClasse());
             planningsHolder.textView_agency_tarif_value.setText(String.valueOf(voyagesListe.get(position).getTarif())+ " XAF");
+            planningsHolder.imageView_rseerve.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(v.getContext(), ReservationActivity.class);
+                    v.getContext().startActivity(i);
+                }
+            });
         }
         else{
             planningsHolder.textView_agence.setBackgroundColor(planningsHolder.textView_agence.getResources().getColor(R.color.colorBlue));
@@ -73,6 +69,7 @@ public class PlanningsHorizontalalAdapter extends RecyclerView.Adapter<Plannings
     public static class PlanningsHolder extends RecyclerView.ViewHolder {
         private ConstraintLayout layout;
         private ImageView imageView_flag;
+        private ImageView imageView_rseerve;
         private TextView textView_agence;
         private TextView textView_categorie;
         private TextView textView_provenance;
@@ -85,6 +82,7 @@ public class PlanningsHorizontalalAdapter extends RecyclerView.Adapter<Plannings
             layout = itemView.findViewById(R.id.layout_template_travel2);
             textView_categorie = itemView.findViewById(R.id.textView_travel_classe_value);
             imageView_flag = itemView.findViewById(R.id.imageView_main_container);
+            imageView_rseerve = itemView.findViewById(R.id.image_view_reserve);
             textView_agence = itemView.findViewById(R.id.textView_agency_value);
             textView_provenance = itemView.findViewById(R.id.textView_agency_provenance_value);
             textView_destination = itemView.findViewById(R.id.textView_agency_destination_value);
